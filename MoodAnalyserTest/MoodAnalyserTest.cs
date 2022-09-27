ac8332f50ae2d1b26f252d4c22d83f53546beacc
surajbridgelabz <surajnsinha@gmail.com>
2022-09-28 00:10:18 +0530

    [suraj] Add: Custom Exception Null Mood Analysis Exception

--- MoodAnalyserTest/MoodAnalyserTest.cs
+++ MoodAnalyserTest/MoodAnalyserTest.cs
@@ -4,18 +4,23 @@ using MoodAnalyzerProblem;
 namespace MoodAnalyserTest
 {
     [TestClass]
-    public class UnitTest1
+    public class MoodAnalyserTest
     {
-        //UC2
-        // TC 2.1: Given null mood Should Return HAPPY
+        //UC3
+        //TC 3.1
         [TestMethod]
-        public void GivenNullMoodShouldReturnHAPPY()
+        public void GivenNullMessage_WhenAnalyse_ShouldReturnHappy()
         {
-            string expected = "HAPPY";
             string message = null;
-            MoodAnalyser moodAnalyse = new MoodAnalyser(message);
-            string mood = moodAnalyse.AnalyseMood();
-            Assert.AreEqual(expected, mood);
+            MoodAnalyser moodAnalyzer = new MoodAnalyser(message);
+            try
+            {
+                string actutal = moodAnalyzer.AnalyseMood();
+            }
+            catch (MoodAnalyserException ex)
+            {
+                Assert.AreEqual(ex.Message, "Message is Null");
+            }
         }
     }
 }
