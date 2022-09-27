e34e089efd2544b4c540922ec8fa5f39fa3f8c7e
surajbridgelabz <surajnsinha@gmail.com>
2022-09-27 15:45:35 +0530

    [suraj] Add: Welcome to Mood Analyzer problems statement

---
+++ MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzerProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer Problems Statement");
+        }
+    }
+}
